691d410e6d33fdccbf843fad5ebfb354436d9aa4
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-09-20 13:00:25 +0700

    feat: implement AdminEditCategory component with form for editing categories

--- client/src/pages/ArticleManagement/AdminEditCategory.jsx
+++ client/src/pages/ArticleManagement/AdminEditCategory.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import AdminPanel from '@/components/ArticleManagement/AdminPanel'
 
+import { FaRegTrashAlt } from "react-icons/fa";
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 const AdminEditCategory = () => {
   return (
-    <div>AdminEditCategory</div>
+    <div className='flex h-screen'>
+      <AdminPanel/>
+
+      {/* Main Edit Category Section */}
+      <main className="flex-1 p-8 bg-gray-50 overflow-auto">
+        <div className="flex justify-between items-center mb-6">
+          <h2 className="text-2xl font-semibold">Edit Category</h2>
+          <Button className="px-8 py-2 rounded-full bg-black text-white">Save</Button>
+        </div>
+        <div className="space-y-7 max-w-md">
+          <div className="relative">
+            <label
+              htmlFor="current-password"
+              className="block text-sm font-medium text-gray-700 mb-1"
+            >
+              Category Name
+            </label>
+            <Input
+              id="current-password"
+              type="password"
+              placeholder="Category name"
+              className="mt-3 py-3 rounded-sm placeholder:text-muted-foreground focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:border-muted-foreground"
+            />
+          </div>
+        </div>
+        <button className="underline underline-offset-2 hover:text-muted-foreground text-sm font-medium flex items-center gap-1 mt-6">
+          <FaRegTrashAlt className="h-5 w-5" />
+          Delete Category
+        </button>
+      </main>
+    </div>
   )
 }
 
