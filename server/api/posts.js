f1316f79f1e251e3b7e4bff887a025d631559ea4
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-09-20 22:39:37 +0700

    refactor: remove console logs for Supabase environment variables and clean up vercel configuration

--- server/api/posts.js
+++ server/api/posts.js
@@ -54,5 +54,3 @@ export default async function handler(req, res) {
   }
 }
 
-console.log("SUPABASE_URL is set:", !!process.env.SUPABASE_URL);
-console.log("SUPABASE_SERVICE_ROLE_KEY is set:", !!process.env.SUPABASE_SERVICE_ROLE_KEY);
